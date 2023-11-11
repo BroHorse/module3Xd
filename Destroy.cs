377cdf2cd1b5945a575a5b2e3d0402c85b4f7e06
BroHorse <150540126+BroHorse@users.noreply.github.com>
2023-11-11 19:41:21 +0300

    Add files via upload

---
+++ Destroy.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    // Delets cube in da project 100 emoji
+    void Start()
+    {
+        Destroy(gameObject);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
